4ffbe5dda37cf49adf306a1497b6afb21599d06f
CxRW <85182216+CxRW@users.noreply.github.com>
2025-01-28 16:49:59 -0600

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,18 @@
 resource "alicloud_ram_account_password_policy" "corporate1" {
   require_lowercase_characters = false
-  require_uppercase_characters = true
-  require_numbers              = true
-  require_symbols              = true
-  hard_expiry                  = true
-  password_reuse_prevention    = 5
-  max_login_attempts           = 15
+  require_uppercase_characters = false
+  require_numbers              = false
+  require_symbols              = false
+  hard_expiry                  = false
+  max_login_attempts           = 10
 }
 
 resource "alicloud_ram_account_password_policy" "corporate2" {
   minimum_password_length = 14
-  require_lowercase_characters = false
-  require_uppercase_characters = false
+  require_lowercase_characters = true
+  require_uppercase_characters = true
   require_numbers              = false
-  require_symbols              = true
+  require_symbols              = false
   hard_expiry                  = true
   password_reuse_prevention    = 5
   max_login_attempts           = 3
